d43bcc452a4405ffc1afc05bb1e1482c25c8e4e8
Bruk Teshome <brukteshome21@gmail.com>
2024-02-02 14:21:12 +0300

    Add different newtwork configrations

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,15 @@
+const {neworkConfig} = require("../helper-hardhat-config")
+
 module.exports = async ({ getNamedAccounts, deployment }) => {
   const { depoy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
+
+  const ethUsdPriceFeedAddress = neworkConfig[chainId]["ethUsdPriceFeed"]
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [address],
+    log: true
+  })
 }
