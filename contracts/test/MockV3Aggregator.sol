23830a7884da5b0140fc0984e110188aa26680f6
Bruk Teshome <brukteshome21@gmail.com>
2024-03-05 15:51:53 +0300

    moved the etherscan configuration outside the networks object

--- contracts/test/MockV3Aggregator.sol
+++ contracts/test/MockV3Aggregator.sol
@@ -1,4 +1,3 @@
 pragma solidity ^0.6.0;
-require("dotenv").config();
 
 import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-deploy")
-require("dotenv").config()
+require("hardhat-deploy");
+require("dotenv").config();
 
-
-/** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
-  // solidity: "0.8.8",
   solidity: {
     compilers: [
       { version: "0.8.8" },
@@ -27,18 +25,16 @@ module.exports = {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
-    etherScan: {
-      apiKey: {
-        sepolia: ETHERSCAN_API_KEY,
-      },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
-    namedAccounts: {
-      deployer: {
-        default: 0,
-      },
-      user: {
-        default: 1
-      }
-    },
+    user: {
+      default: 1
+    }
   }
-}
+};
